5a910141d19f46a4b745175865c1e3ebb0e9f302
rsb251298 <85372087+rsb251298@users.noreply.github.com>
2023-08-15 12:32:24 +0530

    init commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: true },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Logo />
+      <Form />
+      <Travelist />
+      <Stats />
     </div>
+  );
+}
+
+function Logo() {
+  return <h1>🏝️Far Away 🎒</h1>;
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+}
+
+function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuanity] = useState(1);
+  return (
+    <form onSubmit={handleSubmit} className="add-form">
+      <h3>What will you need for your 😍 trip?</h3>
+      <select
+        value={quantity}
+        onChange={(e) => setQuanity(Number(e.target.value))}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => {
+          return (
+            <option key={num} value={num}>
+              {num}
+            </option>
+          );
+        })}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      ></input>
+      <button type="submit">Add</button>
+    </form>
+  );
+}
+function Travelist() {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((list) => (
+          <Item list={list} key={list.id} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ list }) {
+  return (
+    <li>
+      <span style={list.packed ? { textDecorationLine: "line-through" } : {}}>
+        {list.quantity} {list.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>You Have X Items on your list, and you have already packed X(X%)</em>
+    </footer>
   );
 }
 
